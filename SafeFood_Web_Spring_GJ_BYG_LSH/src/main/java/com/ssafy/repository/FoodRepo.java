3d39a69b8f9fc0e8a831131174e8c3572f39e09d
hopui <ishyun1209@naver.com>
2019-04-26 10:41:01 +0900

    일단 수정

--- SafeFood_Web_Spring_GJ_BYG_LSH/src/main/java/com/ssafy/repository/FoodRepo.java
+++ SafeFood_Web_Spring_GJ_BYG_LSH/src/main/java/com/ssafy/repository/FoodRepo.java
@@ -2,6 +2,8 @@ package com.ssafy.repository;
 
 import java.util.List;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.ssafy.model.dto.Food;
@@ -9,9 +11,19 @@ import com.ssafy.model.dto.Food;
 @Repository
 public class FoodRepo
 {
-	public static final String ns = "";
+	public static final String ns = "com.ssafy.model.mapper.Food.";
 	
-//	public List<Food> selectAll() {
-//		
-//	}
+	@Autowired
+	SqlSessionTemplate tmp;
+	
+	public List<Food> selectAll() {
+		String stmt = ns+"selectAll";
+		return tmp.selectList(stmt);
+	}
+	
+	public List<Food> selectMyfoodAll(String email) {
+		String stmt = ns+"selectMyFoodAll";
+		return tmp.selectList(stmt, email);
+	}
+	 
 }
