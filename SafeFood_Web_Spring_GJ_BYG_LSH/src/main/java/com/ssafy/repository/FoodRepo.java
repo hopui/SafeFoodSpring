b42fb4dce49a44b9d361c329dfcf53d36f17d180
hopui <ishyun1209@naver.com>
2019-04-26 11:11:00 +0900

    얍

--- SafeFood_Web_Spring_GJ_BYG_LSH/src/main/java/com/ssafy/repository/FoodRepo.java
+++ SafeFood_Web_Spring_GJ_BYG_LSH/src/main/java/com/ssafy/repository/FoodRepo.java
@@ -1,6 +1,8 @@
 package com.ssafy.repository;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,41 +13,80 @@ import com.ssafy.model.dto.Food;
 
 @Transactional
 @Repository
-public class FoodRepo
-{
+public class FoodRepo {
 	public static final String ns = "com.ssafy.model.mapper.Food.";
-	
+
 	@Autowired
 	SqlSessionTemplate tmp;
-	
+
 	public List<Food> selectAll() {
-		String stmt = ns+"selectAll";
+		String stmt = ns + "selectAll";
 		return tmp.selectList(stmt);
 	}
-	
+
 	public List<Food> selectMyfoodAll(String email) {
-		String stmt = ns+"selectMyFoodAll";
+		String stmt = ns + "selectMyFoodAll";
 		return tmp.selectList(stmt, email);
 	}
-	
+
 	public Food selectCode(int code) {
-		String stmt = ns+"selectCode";
+		String stmt = ns + "selectCode";
 		return tmp.selectOne(stmt, code);
 	}
-	
+
 	public List<Food> selectMaker(String maker) {
-		String stmt = ns+"selectMaker";
+		String stmt = ns + "selectMaker";
 		return tmp.selectList(stmt, maker);
 	}
-	
+
 	public List<Food> selectName(String name) {
-		String stmt = ns+"selectName";
+		String stmt = ns + "selectName";
 		return tmp.selectList(stmt, name);
 	}
-	
+
 	public List<Food> selectMaterial(String mater) {
-		String stmt = ns+"selectMaterial";
+		String stmt = ns + "selectMaterial";
 		return tmp.selectList(stmt, mater);
 	}
-	 
+
+	public Food selectMyFood(String email, String code) {
+		String stmt = ns + "selectMaterial";
+		Map<String, String> menu = new HashMap<String, String>();
+		menu.put("email", email);
+		menu.put("code", code);
+		return tmp.selectOne(stmt, menu);
+	}
+
+	public int insertMyfood(String email, String code, int quantity) {
+		Food food = selectMyFood(email, code);
+		if (food == null) {
+			String stmt = ns + "insertMyFood";
+			Map<String, String> menu = new HashMap<String, String>();
+			menu.put("email", email);
+			menu.put("code", code);
+			menu.put("quantity", String.valueOf(quantity));
+			return tmp.insert(stmt, menu);
+		}else {
+			//return updateMyfood(email, code, food.quantity);
+			return -1;
+		}
+	}
+
+	public int updateMyfood(String email, String code, int quantity) {
+		String stmt = ns + "updateMyFood";
+		Map<String, String> menu = new HashMap<String, String>();
+		menu.put("email", email);
+		menu.put("code", code);
+		menu.put("quantity", String.valueOf(quantity));
+		return tmp.update(stmt, menu);
+	}
+	
+	public int deleteMyfood(String email, String code) {
+		String stmt = ns + "deleteMyFood";
+		Map<String, String> menu = new HashMap<String, String>();
+		menu.put("email", email);
+		menu.put("code", code);
+		return tmp.delete(stmt, menu);
+	}
+
 }
--- SafeFood_Web_Spring_GJ_BYG_LSH/src/main/java/com/ssafy/service/FoodService.java
+++ SafeFood_Web_Spring_GJ_BYG_LSH/src/main/java/com/ssafy/service/FoodService.java
@@ -1,6 +1,58 @@
 package com.ssafy.service;
 
+import java.util.List;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.ssafy.model.dto.Food;
+import com.ssafy.repository.FoodRepo;
+
 public class FoodService
 {
+	@Autowired
+	FoodRepo dao;
 
+	public FoodService(FoodRepo repo) {
+		
+	}
+	public List<Food> selectAll() {
+		return dao.selectAll();
+	}
+
+	public List<Food> selectMyfoodAll(String email) {
+		return dao.selectMyfoodAll(email);
+	}
+
+	public Food selectCode(int code) {
+		return dao.selectCode(code);
+	}
+
+	public List<Food> selectMaker(String maker) {
+		return dao.selectMaker(maker);
+	}
+
+	public List<Food> selectName(String name) {
+		return dao.selectName(name);
+	}
+
+	public List<Food> selectMaterial(String mater) {
+		return dao.selectMaterial(mater);
+	}
+
+	public Food selectMyFood(String email, String code) {
+		return selectMyFood(email, code);
+	}
+
+	public int insertMyfood(String email, String code, int quantity) {
+		return insertMyfood(email, code, quantity);
+	}
+
+	public int updateMyfood(String email, String code, int quantity) {
+		return updateMyfood(email, code, quantity);
+	}
+	
+	public int deleteMyfood(String email, String code) {
+		return deleteMyfood(email, code);
+	}
 }
