21053d4c2c4c83b7fbfc9c8fb2f58d7be2546f8d
hopui <ishyun1209@naver.com>
2019-04-26 09:56:40 +0900

    일단 커밋

--- SafeFood_Web_Spring_GJ_BYG_LSH/src/main/java/com/ssafy/repository/FoodRepo.java
+++ SafeFood_Web_Spring_GJ_BYG_LSH/src/main/java/com/ssafy/repository/FoodRepo.java
@@ -1,6 +1,17 @@
 package com.ssafy.repository;
 
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.ssafy.model.dto.Food;
+
+@Repository
 public class FoodRepo
 {
-
+	public static final String ns = "";
+	
+//	public List<Food> selectAll() {
+//		
+//	}
 }
